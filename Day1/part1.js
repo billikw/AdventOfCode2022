3a9f46dc5e638798c93061aa6b07f91b974832b1
billikw <laokas@protonmail.com>
2022-12-02 16:27:58 +0000

    day 1 part 1 solution added

--- Day1/part1.js
+++ Day1/part1.js
@@ -1,1 +1,27 @@
 // Day 1 (part 1)
+const input = []; // Array.from($0.innerHTML.split("\n"));
+let elvesCalories = [];
+
+countCalories = (source) => {
+  let cals = 0;
+  for (let i = 0; i < source.length; i++) {
+    if (source[i] != "") {
+      cals += Number(source[i]);
+    } else {
+      elvesCalories.push(cals);
+      cals = 0;
+    }
+  }
+};
+
+findLargest = (source) => {
+  let largest = 0;
+  for (let i = 0; i < source.length; i++) {
+    if (source[i] > largest) {
+      largest = source[i];
+    }
+  }
+  return largest;
+};
+
+// Day 1 (part 1) answer: 68292
