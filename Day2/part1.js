a4643c71baf31bec22b0d446ca3b839318541770
billikw <laokas@protonmail.com>
2022-12-05 01:20:36 +0000

    Added Day 2 part 1

---
+++ Day2/part1.js
@@ -0,0 +1,91 @@
+// Day 2 (part 1)
+const input = Array.from(
+  document.getElementsByTagName("pre")[0].innerHTML.split("\n")
+);
+input.pop();
+
+/* Game Info */
+const moves = {
+  A: "Rock",
+  B: "Paper",
+  C: "Scissors",
+  X: "Rock",
+  Y: "Paper",
+  Z: "Scissors",
+};
+const shapes = {
+  Rock: 1,
+  Paper: 2,
+  Scissors: 3,
+};
+const result = {
+  loss: 0,
+  draw: 3,
+  win: 6,
+};
+
+/* Player Info */
+const scores = {
+  elf: 0,
+  human: 0,
+};
+
+playGame = (source) => {
+  let elfPoints = 0;
+  let humanPoints = 0;
+
+  for (let i = 0; i < source.length; i++) {
+    const elfMove = moves[source[i][0]];
+    const humanMove = moves[source[i][2]];
+
+    elfPoints += shapes[elfMove];
+    humanPoints += shapes[humanMove];
+
+    /* Game logic */
+    /* Rock */
+    if (elfMove === "Rock") {
+      if (humanMove === "Scissors") {
+        elfPoints += result.win;
+      } else {
+        if (humanMove === "Rock") {
+          elfPoints += result.draw;
+          humanPoints += result.draw;
+        } else {
+          humanPoints += result.win;
+        }
+      }
+    }
+    /* Scissors */
+    if (elfMove === "Scissors") {
+      if (humanMove === "Paper") {
+        elfPoints += result.win;
+      } else {
+        if (humanMove === "Scissors") {
+          elfPoints += result.draw;
+          humanPoints += result.draw;
+        } else {
+          humanPoints += result.win;
+        }
+      }
+    }
+    /* Paper */
+    if (elfMove === "Paper") {
+      if (humanMove === "Rock") {
+        elfPoints += result.win;
+      } else {
+        if (humanMove === "Paper") {
+          elfPoints += result.draw;
+          humanPoints += result.draw;
+        } else {
+          humanPoints += result.win;
+        }
+      }
+    }
+  }
+  scores.elf = elfPoints;
+  scores.human = humanPoints;
+};
+
+playGame(input);
+console.log(`The scores are - Elf: ${scores.elf} vs Human: ${scores.human}`);
+// Day 2 (part 1) answer: 15422
